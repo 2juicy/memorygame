ac121c5de01564d8e71ec5977750bc8bb3193366
JohnnyC <j0hnnywchiang@gmail.com>
2018-07-11 19:23:13 -0700

    trying to get images to deploy

--- src/components/FriendCard/FriendCard.js
+++ src/components/FriendCard/FriendCard.js
@@ -4,7 +4,7 @@ import "./FriendCard.css";
 const FriendCard = props => (
   <div onClick={() => props.scoreCount(props.name)} className="card">
     <div className="img-container hvr-radial-out">
-      <img alt={props.name} src={props.image} />
+      <img alt={props.name} src={`img/${props.name}.png`} />
     </div>
     <div className="content">
           <strong>{props.name}</strong> 
