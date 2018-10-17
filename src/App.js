d4d692c3ee0df4ce012cae1663abe8955aa59f3a
JohnnyC <j0hnnywchiang@gmail.com>
2018-10-16 23:42:58 -0700

    made clicky game better

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import Title from "./components/Title";
 import Score from "./components/Score";
 import friends from "./friends.json";
 
-
 class App extends Component {
   // Setting this.state.friends to the friends json array
   state = {
     friends,
     count: 0,
     topCount: 0,
-    message: "Click on a card to earn points, but don't click on any more than once!",
-    textcolor: { color: 'White' },
+    message:
+      "Click on a card to earn points, but don't click on any more than once!",
+    textcolor: { color: "White" },
     guesses: []
   };
 
   shuffleFriend = () => {
-    const friends = this.state.friends.sort(function (a, b) { return 0.5 - Math.random() });;
+    const friends = this.state.friends.sort(function(a, b) {
+      return 0.5 - Math.random();
+    });
     // Set this.state.friends equal to the new friends array
     this.setState({ friends });
   };
@@ -35,7 +37,9 @@ class App extends Component {
     } else {
       this.setState({ count: 0 });
       this.setState({ guesses: [] });
-      this.setState({ message: "Incorrect guess! Game Over! Click on any card to start again." });
+      this.setState({
+        message: "Incorrect guess! Game Over! Click on any card to start again."
+      });
       this.setState({ textcolor: { color: "Red" } });
       if (this.state.count > this.state.topCount) {
         this.setState({ topCount: this.state.count });
@@ -58,14 +62,16 @@ class App extends Component {
           message={this.state.message}
           text={this.state.textcolor}
         />
-        {this.state.friends.map(friend => (
-          <FriendCard
-            scoreCount={this.scoreCount}
-            id={friend.id}
-            key={friend.id}
-            name={friend.name}
-          />
-        ))}
+        <div className="box">
+          {this.state.friends.map(friend => (
+            <FriendCard
+              scoreCount={this.scoreCount}
+              id={friend.id}
+              key={friend.id}
+              name={friend.name}
+            />
+          ))}
+        </div>
       </Wrapper>
     );
   }
