ad849de02522b46121ad97b54b9a5108bde0d2f7
JohnnyC <j0hnnywchiang@gmail.com>
2018-10-17 17:27:02 -0700

    hiding span on response

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./Score.css";
 
 const Score = props => (
-    <div className="container">
-        <p className='score'>
-            <span>Score: {props.count}</span>
-            <span>|</span>
-            <span>Top Score: {props.topCount}</span>
-        </p>
-        <p style={props.text} className='message'>{props.message}</p>
-    </div>
+  <div className="container">
+    <p className="score">
+      <span>Score: {props.count}</span>
+      <span className="visi">|</span>
+      <span>Top Score: {props.topCount}</span>
+    </p>
+    <p style={props.text} className="message">
+      {props.message}
+    </p>
+  </div>
 );
 
 export default Score;
