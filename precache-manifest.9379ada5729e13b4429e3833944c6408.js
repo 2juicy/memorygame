d637bdc3906c556669eef774e7979014d9568982
JohnnyC <j0hnnywchiang@gmail.com>
2019-03-04 03:38:12 -0800

    Updates

---
+++ precache-manifest.9379ada5729e13b4429e3833944c6408.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "63d9b396a67cf54d8d9b",
+    "url": "/clickygame/static/js/main.63d9b396.chunk.js"
+  },
+  {
+    "revision": "51f8d5c03b12af818356",
+    "url": "/clickygame/static/js/1.51f8d5c0.chunk.js"
+  },
+  {
+    "revision": "63d9b396a67cf54d8d9b",
+    "url": "/clickygame/static/css/main.d4d2535f.chunk.css"
+  },
+  {
+    "revision": "b0961182f189638715ba4e0c1016d290",
+    "url": "/clickygame/index.html"
+  }
+];
