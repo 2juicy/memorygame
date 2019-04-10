d456906396b90cf2aa109fc7448aaffc36c31499
JohnnyC <j0hnnywchiang@gmail.com>
2019-04-10 06:53:09 -0700

    remade footer for consistency

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,11 @@
 import React from "react";
-
-const footerStyle = {
-  background: "rgba(0, 0, 0, 0.33)",
-  borderTop: "1px solid #E7E7E7",
-  textAlign: "center",
-  padding: "5px",
-  position: "fixed",
-  left: "0",
-  bottom: "0",
-  height: "100px",
-  width: "100%",
-  pointerEvents: "none"
-};
-
-const phantomStyle = {
-  display: "block",
-  padding: "20px",
-  height: "100px",
-  width: "100%"
-};
+import "./Footer.css";
 
 const Footer = props => {
   return (
     <div>
-      <div style={phantomStyle} />
-      <div style={footerStyle}>{props.children}</div>
+      <div className="phantom" />
+      <div className="footer">{props.children}</div>
     </div>
   );
 };
